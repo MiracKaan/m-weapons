6b403f9adaf1b98a2be81c626fe44ec392d299bf
MirageWasHere <63926390+MiracKaan@users.noreply.github.com>
2023-10-15 22:26:47 +0300

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '679-996150c95a1d251a5c0c7841ab2f0276878334f7'
+description 'Slavko - Less Lethal Shotgun'
+files {
+  'weapon_combatshotgun.meta',
+  'weaponheavyshotgun.meta',
+  'weapons_assaultrifle_mk2.meta',
+  'weaponspecialcarbine.meta',
+  'weapons.meta'
+}
+
+data_file 'WEAPONINFO_FILE_PATCH' 'weapon_combatshotgun.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponheavyshotgun.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons_assaultrifle_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponspecialcarbine.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons.meta'
